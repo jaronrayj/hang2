27825a8d17bfdc9b8c4d4deca967fd286835db15
Jaron Johnson <jjaronjohnson14@gmail.com>
2019-02-28 19:42:34 -0700

    Finished product. Gotta catch em all

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,21 +3,21 @@ var wordArray = [
     "Bulbasaur", "Ivysaur", "Venusaur", "Charmander", "Charmeleon", "Charizard", "Squirtle", "Wartortle", "Blastoise", "Caterpie", "Metapod", "Butterfree", "Weedle", "Kakuna", "Beedrill", "Pidgey", "Pidgeotto", "Pidgeot", "Rattata", "Raticate", "Spearow", "Fearow", "Ekans", "Arbok", "Pikachu", "Raichu", "Sandshrew", "Sandslash", "Nidoran", "Nidorina", "Nidoqueen", "Nidoran", "Nidorino", "Nidoking", "Clefairy", "Clefable", "Vulpix", "Ninetales", "Jigglypuff", "Wigglytuff", "Zubat", "Golbat", "Oddish", "Gloom", "Vileplume", "Paras", "Parasect", "Venonat", "Venomoth", "Diglett", "Dugtrio", "Meowth", "Persian", "Psyduck", "Golduck", "Mankey", "Primeape", "Growlithe", "Arcanine", "Poliwag", "Poliwhirl", "Poliwrath", "Abra", "Kadabra", "Alakazam", "Machop", "Machoke", "Machamp", "Bellsprout", "Weepinbell", "Victreebel", "Tentacool", "Tentacruel", "Geodude", "Graveler", "Golem", "Ponyta", "Rapidash", "Slowpoke", "Slowbro", "Magnemite", "Magneton", "Farfetchd", "Doduo", "Dodrio", "Seel", "Dewgong", "Grimer", "Muk", "Shellder", "Cloyster", "Gastly", "Haunter", "Gengar", "Onix", "Drowzee", "Hypno", "Krabby", "Kingler", "Voltorb", "Electrode", "Exeggcute", "Exeggutor", "Cubone", "Marowak", "Hitmonlee", "Hitmonchan", "Lickitung", "Koffing", "Weezing", "Rhyhorn", "Rhydon", "Chansey", "Tangela", "Kangaskhan", "Horsea", "Seadra", "Goldeen", "Seaking", "Staryu", "Starmie", "MrMime", "Scyther", "Jynx", "Electabuzz", "Magmar", "Pinsir", "Tauros", "Magikarp", "Gyarados", "Lapras", "Ditto", "Eevee", "Vaporeon", "Jolteon", "Flareon", "Porygon", "Omanyte", "Omastar", "Kabuto", "Kabutops", "Aerodactyl", "Snorlax", "Articuno", "Zapdos", "Moltres", "Dratini", "Dragonair", "Dragonite", "Mewtwo", "Mew"
 ]
 
+
 var word;
 var pokeDexNumber;
 // get random word
 function randomWord() {
     var random = (Math.floor(Math.random() * wordArray.length));
     word = wordArray[random];
-    pokeDexNumber = random++;
-
-
+    pokeDexNumber = random + 1;
 }
+
+$("#caughtText").hide();
 
 randomWord();
 // Split word into array
 var splitWord = word.split("");
-console.log(word);
 
 var blanksSpaces = [];
 var guesses = [];
@@ -41,8 +41,6 @@ function display() {
     $("#guesses").text("Guesses made: " + guesses);
 }
 
-display();
-initialBlanks();
 // var userInput = userInput.toUpperCase()
 // Add guesses
 document.onkeyup = function (event) {
@@ -68,7 +66,16 @@ document.onkeyup = function (event) {
                 }
 
                 if (!(blanksSpaces.includes("_ "))) {
-                    alert("You Won!")
+                    $("#display").hide();
+                    caught();
+                    $("#caughtText").show().text("Gotcha! Wild " + word + " was caught! Gotta Catch 'em all?");
+                    $("#caughtText").click(function () {
+                        newGame();
+                    });
+
+                    // .text("Gotcha! Wild " + word + " was caught!")
+                    urlCombine();
+                    $("#pokePic").append("<img src=" + pokePic + ">");
                 }
             } else if (guesses.includes(userInput) || i < (splitWord.length - 1)) {
 
@@ -78,29 +85,37 @@ document.onkeyup = function (event) {
                 console.log("guesses left " + guessesLeft);
                 display();
                 if (guessesLeft === 0) {
-                    alert("You lost. The word was " + word);
+                    $("#display").hide();
+                    $("#caughtText").show().text("You missed... The pokémon was " + word + ". Gotta Catch 'em all?");
+                    $("#caughtText").click(function () {
+                        newGame();
+                    });
                 }
             }
         }
     }
 }
 
+function newGame() {
+    $("#appear").hide();
+    $("#caughtText").hide();
+    battle();
+    randomWord();
+    splitWord = word.split("");
+    $("#blanks").empty();
+    correct = 0;
+    guessesLeft = 10;
+    guesses = [];
+    blanksSpaces = [];
+    display();
+    $("#display").show();
+    initialBlanks();
+    console.log(word);
+}
+
 $("#newGame").on("click",
-    function newGame() {
-        randomWord();
-        splitWord = word.split("");
-        $("#blanks").empty();
-        correct = 0;
-        guessesLeft = 10;
-        guesses = [];
-        blanksSpaces = [];
-        display();
-        initialBlanks();
-        console.log(word);
-        urlCombine();
-
-        $("#pokePic").empty().prepend("<img src=" + pokePic + ">")
-
+    function () {
+        newGame()
     })
 
 // Picture
@@ -115,8 +130,20 @@ function urlCombine() {
     }
 }
 
-urlCombine();
-$("#pokePic").prepend("<img src=" + pokePic + ">").hide();
+// Play beginning audio
+var battleMusic = document.getElementById("battle");
 
-console.log(pokeDexNumber);
-console.log(pokePic);
+function battle() {
+    caughtMusic.pause();
+    caughtMusic.currentTime = 0;
+    battleMusic.play();
+}
+
+// Play victory audio
+var caughtMusic = document.getElementById("caught");
+
+function caught() {
+    battleMusic.pause();
+    battleMusic.currentTime = 0;
+    caughtMusic.play();
+}
